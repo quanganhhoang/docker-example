69813d5c47616575ead8d355f872219f26ff9d9b
QA Hoang <qahoang317@gmail.com>
2020-07-29 16:44:26 -0700

    Add frontend code to classify tweets data loaded locally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,95 @@
 import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios'
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+import ResultTable from './ResultTable'
+
+import whitepowerJson from './data/whitepower.json'
 
 function App() {
-    const [input, setInput] = useState('');
+    const [tweetInput, setTweetInput] = useState('');
     const [result, setResult] = useState('');
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [tableResult, setTableResult] = useState({});
 
-    const onSubmit = (e) => {    
+    const onTweetSubmit = (e) => {    
         e.preventDefault()
-    
-        axios.get('http://localhost:8000/predict', {
-            params: {
-                input: input
-            }
-        }).then(res => {
-            setResult(res.data)
-        })
+        classifyText(tweetInput).then(result => setResult(result));
     };
+
+    const classifyText = async (text) => {
+        try {
+            const res = await axios.get('http://localhost:8000/predict', {
+                params: {
+                    input: text
+                }
+            })
+            return res.data;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const toggle = () => setDropdownOpen(prevState => !prevState);
+
+    const handleDropdown = (hashtag) => {
+        console.log(hashtag);
+        
+        const tweets = whitepowerJson.statuses.map(item => item.full_text)
+        let result = {};
+        for (let tweet of tweets) {
+            classifyText(tweet).then(res => result[tweet] = res);
+        }
+        setTableResult(result);
+    }
 
     return (
         <div className="App">
             <h1>Offensive Speech Identifer</h1>
-            <Form onSubmit={onSubmit}>
+            <Form onSubmit={onTweetSubmit}>
                 <FormGroup>
                     <Label for="exampleText">Enter any text here:</Label>
                     <Input 
                         type="textarea" 
                         name="text" 
                         id="exampleText"
-                        onChange={e => setInput(e.target.value)}
+                        onChange={e => setTweetInput(e.target.value)}
                     />
                 </FormGroup>
                 <Button type="submit">Submit</Button>
             </Form>
             <br></br>
             <h2>Result: {result}</h2>
+
+            <h1>Twitter Hashtag Trending Analysis</h1>
+
+            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                <DropdownToggle caret>
+                    Select search files
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem header>hashtags</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem
+                        onClick = {() => handleDropdown("whitepower")}
+                    >
+                        #whitepower
+                    </DropdownItem>
+                    <DropdownItem
+                        onClick = {() => handleDropdown("blm")}
+                    >
+                        #blm
+                    </DropdownItem>
+                    <DropdownItem
+                        onClick = {() => handleDropdown("seattleprotests")}
+                    >
+                        #seattleprotests
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+            <br></br>
+            <ResultTable data={tableResult}/>
         </div>
     )
 }
---
+++ frontend/src/ResultTable.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Table } from 'reactstrap'
+
+function ResultTable(props) {
+    const result = props.data;
+    let tableRows = [];
+    let i = 1;
+    
+    for (const [key, value] of Object.entries(result)) {
+        tableRows.push(
+            <tr>
+                <th scope="row">{i++}</th>
+                <th>{key}</th>
+                <th>{value}</th>
+            </tr>
+        )
+    }
+
+    return (
+        <div>
+            <Table>
+                <thead>
+                    <tr>
+                    <th>#</th>
+                    <th>Tweet</th>
+                    <th>Classification Result</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableRows}
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+
+export default ResultTable
