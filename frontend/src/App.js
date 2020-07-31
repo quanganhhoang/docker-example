2fc71e1cb3a7d6ea56c08a676ef18b22c6bdb4e3
Nancy <tran.nan@husky.neu.edu>
2020-07-31 15:21:44 -0700

    adding pie chart to front end to display percentage of offensive tweets from hashtag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,128 +1,138 @@
-import React, { useState } from 'react';
-import './App.css';
-import axios from 'axios'
-import { Button, Form, FormGroup, Label, Input, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
-import ResultTable from './ResultTable'
+import ResultTable from "./ResultTable";
+import ResultChart from "./ResultChart";
 
-import whitepowerJson from './data/whitepower.json'
-import blmJson from './data/blm.json'
-import seattleprotestsJson from './data/seattleprotests.json'
+import whitepowerJson from "./data/whitepower.json";
+import blmJson from "./data/blm.json";
+import seattleprotestsJson from "./data/seattleprotests.json";
 
 function App() {
-    const INITIAL_STATE = {
-        'hi': "Not offensive speech",
-        'this frontend is shit': 'Offensive speech'
+  const INITIAL_STATE = {
+    hi: "Not offensive speech",
+    "this frontend is shit": "Offensive speech",
+  };
+
+  const [tweetInput, setTweetInput] = useState("");
+  const [result, setResult] = useState("");
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [tableResult, setTableResult] = useState(INITIAL_STATE);
+  const [dropdownOption, setDropdownOption] = useState("Select hashtag");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onTweetSubmit = (e) => {
+    e.preventDefault();
+    classifyText(tweetInput).then((result) => setResult(result));
+  };
+
+  const classifyText = async (text) => {
+    try {
+      const res = await axios.get("http://localhost:8000/predict", {
+        params: {
+          input: text,
+        },
+      });
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  const handleDropdown = async (hashtag) => {
+    setDropdownOption("#" + hashtag);
+    setIsLoading(true);
+    let tweets;
+    switch (hashtag) {
+      case "whitepower":
+        tweets = whitepowerJson.statuses.map((item) => item.full_text);
+        break;
+      case "blm":
+        tweets = blmJson.statuses.map((item) => item.full_text);
+        break;
+      case "seattleprotests":
+        tweets = seattleprotestsJson.statuses.map((item) => item.full_text);
+        break;
+      default:
+        break;
     }
 
-    const [tweetInput, setTweetInput] = useState('');
-    const [result, setResult] = useState('');
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const [tableResult, setTableResult] = useState(INITIAL_STATE);
-    const [dropdownOption, setDropdownOption] = useState('Select hashtag');
-    const [isLoading, setIsLoading] = useState(false)
-
-    const onTweetSubmit = (e) => {    
-        e.preventDefault()
-        classifyText(tweetInput).then(result => setResult(result));
-    };
-
-    const classifyText = async (text) => {
-        try {
-            const res = await axios.get('http://localhost:8000/predict', {
-                params: {
-                    input: text
-                }
-            })
-            return res.data;
-        } catch (err) {
-            console.log(err);
-        }
+    let result = {};
+    for (let tweet of tweets) {
+      const res = await classifyText(tweet);
+      result[tweet] = res;
     }
+    setTableResult(result);
+    setIsLoading(false);
+  };
 
-    const toggle = () => setDropdownOpen(prevState => !prevState);
+  return (
+    <div className="App" style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ margin: "50px 10px 50px 10px" }}>
+        <h1>Offensive Speech Identifer</h1>
+        <Form onSubmit={onTweetSubmit}>
+          <FormGroup>
+            <Label for="exampleText">Enter any text here:</Label>
+            <Input
+              type="textarea"
+              name="text"
+              id="exampleText"
+              onChange={(e) => setTweetInput(e.target.value)}
+            />
+          </FormGroup>
+          <Button type="submit">Submit</Button>
+        </Form>
+        <br></br>
+        <h2>Result: {result}</h2>
+      </div>
 
-    const handleDropdown = async (hashtag) => {
-        setDropdownOption('#' + hashtag);
-        setIsLoading(true);
-        let tweets;
-        switch (hashtag) {
-            case 'whitepower':
-                tweets = whitepowerJson.statuses.map(item => item.full_text)
-                break;
-            case 'blm':
-                tweets = blmJson.statuses.map(item => item.full_text)
-                break;
-            case 'seattleprotests':
-                tweets = seattleprotestsJson.statuses.map(item => item.full_text)
-                break;
-            default:
-                break;
-        }
-        
-        let result = {};
-        for (let tweet of tweets) {
-            const res = await classifyText(tweet);
-            result[tweet] = res;
-        }
-        setTableResult(result);
-        setIsLoading(false);
-    }
+      <div style={{ margin: "50px 0px 50px 10px" }}>
+        <h1>Twitter Hashtag Trending Analysis</h1>
 
-    return (
-        <div className="App" style={{display: 'flex', flexDirection: 'column'}}>
-            <div style={{margin: '50px 10px 50px 10px'}}>
-                <h1>Offensive Speech Identifer</h1>
-                <Form onSubmit={onTweetSubmit}>
-                    <FormGroup>
-                        <Label for="exampleText">Enter any text here:</Label>
-                        <Input 
-                            type="textarea" 
-                            name="text" 
-                            id="exampleText"
-                            onChange={e => setTweetInput(e.target.value)}
-                        />
-                    </FormGroup>
-                    <Button type="submit">Submit</Button>
-                </Form>
-                <br></br>
-                <h2>Result: {result}</h2>
-            </div>
+        <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+          <DropdownToggle caret>{dropdownOption}</DropdownToggle>
 
-            <div style={{margin: '50px 10px 50px 10px'}}>
-                <h1>Twitter Hashtag Trending Analysis</h1>
+          <DropdownMenu>
+            <DropdownItem header>hashtags</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem onClick={() => handleDropdown("whitepower")}>
+              #whitepower
+            </DropdownItem>
+            <DropdownItem onClick={() => handleDropdown("blm")}>
+              #blm
+            </DropdownItem>
+            <DropdownItem onClick={() => handleDropdown("seattleprotests")}>
+              #seattleprotests
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <br></br>
 
-                <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-                    <DropdownToggle caret>
-                        {dropdownOption}
-                    </DropdownToggle>
-                    
-                    <DropdownMenu>
-                        <DropdownItem header>hashtags</DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem
-                            onClick = {() => handleDropdown("whitepower")}
-                        >
-                            #whitepower
-                        </DropdownItem>
-                        <DropdownItem
-                            onClick = {() => handleDropdown("blm")}
-                        >
-                            #blm
-                        </DropdownItem>
-                        <DropdownItem
-                            onClick = {() => handleDropdown("seattleprotests")}
-                        >
-                            #seattleprotests
-                        </DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-                <br></br>
-                
-                {isLoading ? <p>Loading...</p> : <ResultTable data={tableResult}/>}
-            </div>
-        </div>
-    )
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <ResultChart data={tableResult} />
+            <ResultTable data={tableResult} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/ResultChart.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Plot from "react-plotly.js";
+
+function ResultChart(props) {
+  const result = props.data;
+  let numOffensive = 0;
+  let numNonOffensive = 0;
+
+  for (const [key, value] of Object.entries(result)) {
+    if (value === "Not offensive speech") {
+        numNonOffensive += 1;
+    } else {
+        numOffensive += 1;
+    }
+  }
+
+  return (
+    <Plot
+      data={[
+        {
+          values: [numNonOffensive, numOffensive],
+          labels: ["Not offensive speech", "Offensive speech"],
+          type: "pie",
+        },
+      ]}
+      layout={{ width: 500, height: 500}}
+    />
+  );
+}
+
+export default ResultChart;
